178e9aba600837cb5aa183182dbc8df8dec95cb9
Riddhiman <shubro17@outlook.com>
2023-03-09 23:42:39 +0530

    admin routes added

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -27,3 +27,53 @@ router.get('/pendingApprovals',async (req,res)=>{
         })
     }
 })
+
+router.post('/approve',async (req,res)=>{
+    try{
+        const approveEvent = await events.updateOne({
+            bookingId:req.body.bookingId
+        },{
+            eventStatus:"approved"
+        })
+        if(approveEvent){
+            res.status(200).send({
+                Message:'Success'
+            })
+        }
+        else{
+            res.status(200).send({
+                Message:'Failure'
+            })
+        }
+    }catch(e){
+        res.status(500).send({
+            Message:'Error'
+        })
+    }
+})
+
+router.post('/cancel',async (req,res)=>{
+    try{
+        const approveEvent = await events.updateOne({
+            bookingId:req.body.bookingId
+        },{
+            eventStatus:`cancelled - ${req.body.reason}`
+        })
+        if(approveEvent){
+            res.status(200).send({
+                Message:'Success'
+            })
+        }
+        else{
+            res.status(200).send({
+                Message:'Failure'
+            })
+        }
+    }catch(e){
+        res.status(500).send({
+            Message:'Error'
+        })
+    }
+})
+
+module.exports = router
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 const organizer_router = require('../routes/organizer.router')
+const admin_router = require('../routes/admin.router')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -14,6 +15,6 @@ app.set('view engine','ejs')
 app.use(cors());
 app.use(express.json());
 app.use(organizer_router);
-// app.use(admin_router);
+app.use(admin_router);
 app.use(express.static(`${__dirname}/staticfiles`))
 module.exports = app
